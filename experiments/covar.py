96b93656105ab3cbbdf8c028f1a29ef226083b57
Amir Shaikhha <amir.shaikhha@cs.ox.ac.uk>
2022-02-04 20:22:03 +0000

    prepared for experiments

--- experiments/covar.py
+++ experiments/covar.py
@@ -1,13 +1,4 @@
-# Copyright 2018 Side Li and Arun Kumar
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from covar_shared import * 
 
 import numpy as np
 import time
@@ -18,64 +9,49 @@ import pandas as pd
 
 trials = 10
 # nr = 1000000
-ds = 1
-f = 1
+ds = DS
+# f = 5
 
 # Cross product
 print "start tesing cross product (m.T * m)"
-numpy_res = []
-morpheus_res = []
-# for t in range(1, 21, 4):
-# for t in range(20, 21, 4):
-for t in range(2, 3, 4):
+
+def run_covar(f, t):
+    numpy_res = []
+    morpheus_res = []
     print "Crossprod, feature ratio:", f, "tuple ratio", t
-    R_df = pd.read_csv("R_%d.csv" % t, delimiter='|', header=None).to_numpy()
-    S_df = pd.read_csv("S_%d.csv" % t, delimiter='|', header=None).to_numpy()
+    R_df = pd.read_csv("data/R_%d_%d.csv" % (f, t), delimiter='|', header=None).to_numpy()
+    S_df = pd.read_csv("data/S_%d_%d.csv" % (f, t), delimiter='|', header=None).to_numpy()
+    dr = ds * f
     nr = R_df.shape[0]
     ns = S_df.shape[0]
-    r = [R_df[:,1:2]]
-    s = S_df[:,1:2]
+    r = [R_df[:,1:(dr+1)]]
+    s = S_df[:,1:(ds+1)]
     k = [S_df[:,0]]
 
     # T = np.hstack((s, r[0][k[0]]))
+    T = np.hstack((s, r[0][k[0]],r[0][k[0]]))
     # print T
     # print np.dot(T.T, T)
     normalized_matrix = nm.NormalizedMatrix(s, r, k)
     # print (normalized_matrix.T * normalized_matrix)
     for _ in range(trials):
         m_start = time.time()
-        T = np.hstack((s, r[0][k[0]]))
-        np.dot(T.T, T)
-        # tmp0 = T.T * T
+        tmp0 = np.dot(T.T, T)
         m_end = time.time()
         n_start = time.time()
         tmp1 = normalized_matrix.T * normalized_matrix
         n_end = time.time()
-        # avg.append((m_end - m_start) / (n_end - n_start))
         numpy_res.append((m_end - m_start) * 1000)
         morpheus_res.append((n_end - n_start) * 1000)
 
-    # print (sum(avg) - min(avg) - max(avg)) / (trials - 2)
-    # result.append((sum(avg) - min(avg) - max(avg)) / (trials - 2))
-# total.append(result)
-print numpy_res
-print morpheus_res
+    print np.average(numpy_res)
+    print np.average(morpheus_res)
 
-# layout = go.Layout(
-#     title='Crossprod speedup',
-#     xaxis=dict(
-#         title='Feature Ratio'
-#     ),
-#     yaxis=dict(
-#         title='Tuple Ratio'
-#     )
-# )
-# trace = go.Heatmap(z=total,
-#                    x=range(1, 21),
-#                    y=range(1, 5),
-#                    zmin=0,
-#                    zmax=6)
-# data = [trace]
-# fig = go.Figure(data=data, layout=layout)
-# plotly.offline.plot(fig, filename='crossprod.html', show_link=False)
+for t in TS:
+# for t in range(20, 21, 4):
+# for t in range(2, 3, 4):
+    run_covar(FIXED_F, t)
+
+for f in FS:
+    run_covar(f, FIXED_T)
 
--- experiments/covar_dataset.py
+++ experiments/covar_dataset.py
@@ -1,26 +1,25 @@
 import numpy as np
 import time
 import pandas as pd
+from covar_shared import * 
 
 
-nr = 1000000
+ns = NS
 # nr = 1000
 # nr = 10
-ds = 1
-f = 1
+ds = DS
+# f = 10
 
 # Cross product
 print "start generating dataset"
 
-# for t in range(1, 21, 4):
-for t in range(20, 21, 4):
-# for t in range(2, 3, 4):
+def generate_dataset(f, t):
     print "feature ratio:", f, "tuple ratio", t
     dr = ds * f
-    ns = nr * t
+    nr = ns / t
 
-    s = np.random.randint(1000, size=(ns, ds))
-    r = np.random.randint(1000, size=(nr, dr))
+    s = np.random.randint(100, size=(ns, ds))
+    r = np.random.randint(100, size=(nr, dr))
     num = np.random.randint(nr, size=ns)
     # while (max(num) != nr - 1):
     #     num = np.random.randint(nr, size=ns)
@@ -31,8 +30,15 @@ for t in range(20, 21, 4):
     # dt.to_csv('foo.csv', mode='a', index=True)
     idx = np.arange(0, nr)[..., None]
     mat = np.hstack((idx, r))
-    np.savetxt("R_%d.csv" % t, mat,"%d|%d")
+    np.savetxt("data/R_%d_%d.csv" % (f, t), mat,"%d|" * dr + "%d")
     idx = k[..., None]
     mat = np.hstack((idx, s))
-    np.savetxt("S_%d.csv" % t, mat,"%d|%d")
+    np.savetxt("data/S_%d_%d.csv" % (f, t), mat,"%d|" * ds + "%d")
 
+for t in TS:
+# for t in range(20, 21, 4):
+# for t in range(2, 3, 4):
+    generate_dataset(FIXED_F, t)
+
+for f in FS:
+    generate_dataset(f, FIXED_T)
